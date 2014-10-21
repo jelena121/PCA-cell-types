e08fe910266bff418c697bbad812f69a2f9f4539
Jelena <jelena121@gmail.com>
2014-10-21 03:48:15 +0100

    now functional plotting script

--- scripts/parse_data.py
+++ scripts/parse_data.py
@@ -3,15 +3,33 @@ import sys
 import matplotlib
 from numpy import linalg as LA
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
 columns= ''
+factormap = {}
+factorlist = {}
+
+# metadata reading in
+with open(sys.argv[2]) as f:
+	f.readline() 
+	while True:
+		line = f.readline()
+		line.strip()
+		if len(line) == 0:
+			break
+		tmp = line.split("\t")
+		factormap[tmp[0]] = tmp[32]
+		factorlist[tmp[32]] = True
+
 
 with open(sys.argv[1]) as f: 
 	content = f.readline()
 	columns = content.split("\t")
-	print columns
+	del columns[0]
 
 print len(columns)
+
+
 	
 # took out len(columns)
 data = numpy.loadtxt(sys.argv[1], delimiter="\t", skiprows=2, usecols=range(1,10))
@@ -32,14 +50,23 @@ print eigenpairs
 def pcaplot(dim1=0,dim2=1, outputfile=None):
 	eig1 = eigenpairs[dim1][1]
 	eig2 = eigenpairs[dim2][1]
-	x = []
-	y = []
-	for i in range(0,data.shape[1]):
-		x.append(numpy.dot(data[:,i], eig1))
-		y.append(numpy.dot(data[:,i], eig2))
-	print x
-	print y
-	plt.scatter(x, y)
+	colors = iter(cm.rainbow(numpy.linspace(0, 1, len(factorlist.keys()))))
+	plt.figure()
+	for factorname in factorlist.keys():
+		x = []
+		y = []
+		for i in range(0, data.shape[1]):
+			columnname = columns[i].strip("\"")
+			if factormap[columnname] != factorname:
+				continue
+			
+			
+			x.append(numpy.dot(data[:,i], eig1))
+			y.append(numpy.dot(data[:,i], eig2))
+		print x
+		print y
+		plt.scatter(x, y, label=factorname, color=next(colors))
+	plt.legend()
 	if (outputfile):
 		plt.savefig(outputfile)
 	else:
